46566b564e747f0ba2d6780ec8b1a95fe0083702
sasikumardurai <sasikumar.durai@meritgroup.co.uk>
2020-10-28 20:54:56 +0530

    sample

--- myfunction/index.js
+++ myfunction/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Changes from Github 2!'),
+        body: JSON.stringify('Changes from Github 4!'),
     };
     return response;
     

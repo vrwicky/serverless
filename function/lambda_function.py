cd69f0233c8247426eb11abdaef0356fa53d5db9
traj231 <73581126+traj231@users.noreply.github.com>
2020-10-29 11:36:03 +0530

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hello(event, context):
+def lambda_handler(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully! ",
         "input": event

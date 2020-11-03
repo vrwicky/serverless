0dcc2667c626f2743c38f44fa36dc7578281cb5a
vrwicky <72515989+vrwicky@users.noreply.github.com>
2020-11-03 13:40:24 +0530

    Update index.js

--- myfunction/index.js
+++ myfunction/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Changes from Github 6!'),
+        body: JSON.stringify('Changes from Github 7!'),
     };
     return response;
     
